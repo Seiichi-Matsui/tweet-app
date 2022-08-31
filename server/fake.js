8ea32c9e4d9de3cf0f9bd161781902f68631226e
Seiichi-Matsui <s-matsui@zima-corp.com>
2022-08-31 19:46:33 +0900

    ビルド＆デプロイ

--- server/fake.js
+++ server/fake.js
@@ -4,10 +4,7 @@ const Coment = require("./model/coment")
 class FakeDb {
     constructor() {
         this.coments = [
-        {userName: '松井', userComent: '寒くなったな〜'},
-        {userName: '平野', userComent: 'そうでもない'},
-        {userName: '木村', userComent: '確かに'},
-        {userName: '市川', userComent: 'ってか暑い'},
+        {userName: '松井', userComent: 'ようこそ！！'},
         ]
     }
 
